a89771949b4a32c79c1f485b0dbf4bdf6e996cfe
Hergusbergus <zane.ammerman@gmail.com>
2025-07-24 20:04:05 -0600

    Audio Manager
    Added an audio manager with passive BGM

---
+++ Assets/AR/Xane/SoundManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public enum SoundType
+{
+    ASGORE_PASSIVE
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1f)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+
+    // Plays a sound in a loop until stopped
+    public static void PlayLoopingSound(SoundType sound, float volume = 1f)
+    {
+        instance.audioSource.clip = instance.soundList[(int)sound];
+        instance.audioSource.volume = volume;
+        instance.audioSource.loop = true;
+        instance.audioSource.Play();
+    }
+
+    // Stops the currently looping sound
+    public static void StopLoopingSound()
+    {
+        instance.audioSource.Stop();
+        instance.audioSource.loop = false;
+        instance.audioSource.clip = null;
+    }
+}
