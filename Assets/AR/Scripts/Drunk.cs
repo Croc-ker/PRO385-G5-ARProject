2c3643e0368646a6766d1294df4434d078df26d1
Croc-ker <123499932+Croc-ker@users.noreply.github.com>
2025-07-29 11:41:34 -0600

    drunk effect redo

--- Assets/AR/Scripts/Drunk.cs
+++ Assets/AR/Scripts/Drunk.cs
@@ -6,27 +6,23 @@ using UnityEngine.Rendering.HighDefinition;
 public class Drunk : MonoBehaviour
 {
     [SerializeField] private Volume drunkVolume;
-    [SerializeField] private Transform playerTransform;
-    private LensDistortion lensDistortion;
-    private VolumeProfile profile;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        profile = drunkVolume.sharedProfile;
+        drunkVolume.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(profile.TryGet<LensDistortion>(out lensDistortion))
-        {
-            float minx = 0.3f;
-            float maxx = 0.7f;
-        }
     }
 
     public void OnClick()
     {
-        Instantiate(drunkVolume, playerTransform.position, Quaternion.identity);
+        drunkVolume.enabled = true;
+        Invoke("DisableDrunk", 5f);
+    }
+    private void DisableDrunk()
+    {
+        drunkVolume.enabled = false;
     }
 }
